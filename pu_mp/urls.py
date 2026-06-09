dd380250cd00a25d6f0fdb5f31ffb8808e9d5801
Nene <puconnect1@gmail.com>
2026-06-09 07:12:46 +0000

    better seos

--- pu_mp/urls.py
+++ pu_mp/urls.py
@@ -7,7 +7,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    # SEO
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
+
     # Admin
     path('admin/', admin.site.urls),
     
